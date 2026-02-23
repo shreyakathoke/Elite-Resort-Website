7a02971b3f0f5ccbcb95c53c719794ab026d0a2b
shreyakathoke <kathokeshreya03@gmail.com>
2026-02-23 14:30:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+import HomePage from "./pages/Home/HomePage";
+import ContactPage from "./pages/Contact/ContactPage";
+import AboutPage from "./pages/About/AboutPage";
+import ServicePage from "./pages/Service/ServicePage";
+import GalleryPage from "./pages/Gallery/GalleryPage";
+
+import ScrollToTop from "./components/common/Scrollpage";
+import ProfilePage from "./pages/users/ProfilePage";
+
+
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+
+
+
+export default function App() {
+  return (
+    <>
+    <ScrollToTop/>
+      <Header />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/service" element={<ServicePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          
+          
+          <Route path="/profile" element={<ProfilePage />} />
+
+
+        </Routes>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://resort-production.up.railway.app", // ✅ correct
+  withCredentials: false,
+});
+
+// Attach token (if using JWT)
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
